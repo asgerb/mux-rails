93ae1004d745b9377bc0d4887b5994fb5bd3408e
asgerb <a@asgerbehnckejacobsen.dk>
2019-07-09 12:57:00 +0200

    Fix controller protect_from_forgery issue

--- app/controllers/mux/events_controller.rb
+++ app/controllers/mux/events_controller.rb
@@ -1,6 +1,9 @@
 module Mux
   class EventsController < ActionController::Base
-    # QUESTION: Do we need protect_from_forgery?
+    # QUESTION: Is this the right way to handle it?
+    if ::Rails.application.config.action_controller.default_protect_from_forgery
+      skip_before_action :verify_authenticity_token
+    end
 
     def create
       Mux::Notifications.instrument(event)
